090e63c5c739a687979e7326e49c7432eed596e4
José Magalhães <joses.magalhaes@hotmail.com>
2024-03-10 20:12:57 -0300

    :construction: Definição de variáveis do Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private int pontos;
+    public int outrosPontos;
+    public float peso;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Testando game");
     }
 
     // Update is called once per frame
